f563578533c284bd1f9b12a71f496aae953c7b4f
nabylmuhdirfan <muhammadirfannabyl@gmail.com>
2021-01-14 20:11:41 +0800

    Create home.php
    Home page for logged in user.

---
+++ home.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>STASYS - Home</title>
+		<link rel="stylesheet" type="text/css" href="styles.css">
+		<script src="https://kit.fontawesome.com/2ba9e2652f.js" crossorigin="anonymous"></script>
+	</head>
+	<body>
+		<nav>
+			<h1>STASYS</h1>
+			<a href="user_page.php"><i class="fa fa-user-circle-o" aria-hidden="true"></i> ?username?</a>
+			<a href="event_page.php">Events</a>
+			<a href="logout.php">Log Out</a>
+		</nav>
+	</body>
+</html>
