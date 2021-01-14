299c63286aea78dbd3d2dbee7dc65e569fcea63e
Edrie <56837896+andrianedrie@users.noreply.github.com>
2021-01-14 21:58:15 +0800

    Update register.php

--- register.php
+++ register.php
@@ -3,40 +3,52 @@
         <h1>STASYS - REGISTER</h1>
 
         <form method='post' action='register.php'>
+        <table>
+            <tr>
+                <td>User Name</td>
+                <td><input type="text" class="form-control" name="username" id="username" required="required" maxlength="50"></td>
+            </tr>
 
-            <div class="form-group">
-                <label for="lname">User Name:</label>
-                <input type="text" class="form-control" name="username" id="username" required="required" maxlength="50">
-            </div>
-            <div class="form-group">
-                <label for="lname">Name:</label>
-                <input type="text" class="form-control" name="username" id="username" required="required" maxlength="50">
-            </div>
-            <div class="form-group">
-                <label for="email">Email address:</label>
-                <input type="email" class="form-control" name="email" id="email" required="required" maxlength="50">
-            </div>
-            <div class="form-group">
-                <label for="password">Password:</label>
-                <input type="password" class="form-control" name="password" id="password" required="required" maxlength="50">
-            </div>
-            <div class="form-group">
-                <label for="pwd">Confirm Password:</label>
-                <input type="password" class="form-control" name="confirmpassword" id="confirmpassword" onkeyup='' required="required" maxlength="80">
-            </div>
-            <div class="form-group">
-                <label for="lname">Student ID:</label>
-                <input type="text" class="form-control" name="username" id="username" required="required" maxlength="50">
-            </div>
-            <div class="form-group">
-                <label for="lname">Program:</label>
-                <input type="text" class="form-control" name="username" id="username" required="required" maxlength="50">
-            </div>
-            <div class="form-group">
-                <label for="lname">Part:</label>
-                <input type="text" class="form-control" name="username" id="username" required="required" maxlength="50">
-            </div>
-            <button type="submit" name="btnsignup" class="btn btn-default">Submit</button>
+            <tr>
+                <td>Name</td>
+                <td><input type="text" class="form-control" name="username" id="username" required="required" maxlength="50"></td>
+            </tr>
+            
+            <tr>
+                <td>Email Address</td>
+                <td><input type="email" class="form-control" name="email" id="email" required="required" maxlength="50"></td>
+            </tr>
+            
+            <tr>
+                <td>Password</td>
+                <td><input type="password" class="form-control" name="password" id="password" required="required" maxlength="50"></td>
+            </tr>
+            
+            <tr>
+                <td>Cofnrim Password</td>
+                <td><input type="password" class="form-control" name="confirmpassword" id="confirmpassword" onkeyup='' required="required" maxlength="80"></td>
+            </tr>
+            
+            <tr>
+                <td>Student ID</td>
+                <td><input type="text" class="form-control" name="username" id="username" required="required" maxlength="50"></td>
+            </tr>
+            
+            <tr>
+                <td>Program</td>
+                <td><input type="text" class="form-control" name="username" id="username" required="required" maxlength="50"></td>
+            </tr>
+            
+            <tr>
+                <td>Part</td>
+                <td><input type="text" class="form-control" name="username" id="username" required="required" maxlength="50"></td>
+            </tr>
+            
+            <tr>
+                <td></td>
+                <td><button type="submit" name="btnsignup" class="btn btn-default">Submit</button></td>
+            </tr>
+            
         </form>
     </body>
 </html>
