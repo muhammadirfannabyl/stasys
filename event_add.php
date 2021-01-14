cd720167d9b04fed7c3c06e7a2c1b6b719801061
nabylmuhdirfan <muhammadirfannabyl@gmail.com>
2021-01-15 01:39:03 +0800

    Update event_add.php

--- event_add.php
+++ event_add.php
@@ -16,7 +16,7 @@
 		$time=$_POST['time'];
 		$quota=$_POST['quota'];
 		$datetime = date('Y-m-d H:i:s', strtotime("$date $time"));
-		$sql="INSERT INTO event (name, desc, when, quota, u_id) VALUES ('{$name}', '{$desc}', '{$datetime}', '{$quota}', '{$uid}')";
+		$sql="INSERT INTO event (name, desc, when1, quota, u_id) VALUES ('{$name}', '{$desc}', '{$datetime}', '{$quota}', '{$uid}')";
 
 		$result=mysqli_query($conn,$sql);
 
