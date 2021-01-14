ec65e65385905ccd97f1524284a6e368344b1b41
Edrie <56837896+andrianedrie@users.noreply.github.com>
2021-01-14 22:45:43 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli("localhost","u726414194_stasys", "qwer1234", "u726414194_stasys");
+	$conn = new mysqli("localhost","u726414194_stasy", "qwer1234", "u726414194_stasys");
 	if($conn->connect_error){
 		die("Connection failed: " .$conn->connect_error);
 	}
