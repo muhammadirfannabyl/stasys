a00a42904b954a0a6619fecbe50a7a85cb117ddf
Edrie <56837896+andrianedrie@users.noreply.github.com>
2021-01-14 19:39:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
-<!DOCTYPE HTML>
+<?php
+    session_start();
+    require_once "config.php";
+    //Connect to database
+?>
+
 <html>
     <head><title>STASYS - Login</title></head>
     <body>
         <h1>STASYS</h1>
         <p>Login</p>
-        <form>
-            <table>
-                <tr><td>Username: </td><td><input type="text"></td></tr>
-                <tr><td>Password: </td><td><input type="text"></td></tr>
-            </table>
-            <button type="submit">Log In</button><input type="button" name="register" value="Register"/>
-        </form>
+        <form name="form1" method="post" action="login.php">
+        <table>
+        <tr>
+        <td>User Name</td>
+        <td><input type="text" placeholder="Username" name="username" /></td>
+        </tr>
+        <tr>
+        <td>Password</td>
+        <td><input type="password" placeholder="Password" name="password" /></td>
+        </tr>
+        <tr>
+        <td></td>
+        <td>
+        <br><button type="submit" name="Submit" ><strong>Login</strong></button>
+        &nbsp;<button type="submit" name="register" ><strong>Register</strong></button>
+        </td>
+        </tr>
+        </table></br></br>
     </body>
 </html>
