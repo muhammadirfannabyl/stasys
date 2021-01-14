d371c85d1baec47b426f59e802ff65f611130ae2
nabylmuhdirfan <muhammadirfannabyl@gmail.com>
2021-01-15 00:39:49 +0800

    Update home.php

--- home.php
+++ home.php
@@ -43,7 +43,7 @@
 		<nav align="center" class="align-center">
 			<h1>STASYS</h1>
 			<a href="user_profile.php"><i class="fa fa-user-circle-o" aria-hidden="true"></i> <?php echo $rowuser['un']; ?></a>&nbsp;&nbsp;&nbsp;
-			<a href="event_add.php"><i class="fa fa-calendar" aria-hidden="true"></i> Add Event</a>&nbsp;&nbsp;&nbsp;
+			<a href="event.php"><i class="fa fa-calendar" aria-hidden="true"></i> Add Event</a>&nbsp;&nbsp;&nbsp;
 			<a href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Log Out</a>
 		</nav>
 		<!--List existing events from Database-->
