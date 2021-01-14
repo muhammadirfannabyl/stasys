032ef10461739635e92b9fc81f2a6fcc49dd457f
nabylmuhdirfan <muhammadirfannabyl@gmail.com>
2021-01-14 23:43:39 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,41 +1,32 @@
 <?php
+	require_once "config.php";
+	session_start();
 
-require_once "config.php";
-session_start();
-
-if(isset($_POST['Submit']))
-{
-	//post user id and password
-	$un = mysqli_real_escape_string($conn, $_POST['username']);
-	$pw = mysqli_real_escape_string($conn, $_POST['password']);
+	if(isset($_POST['Submit']))	{
+		//post user id and password
+		$un = mysqli_real_escape_string($conn, $_POST['username']);
+		$pw = mysqli_real_escape_string($conn, $_POST['password']);
 
 
-	//query to find user
-	$sql = "SELECT * FROM user WHERE un = '$un' AND pw = '$pw'";
-	$query = $conn->query($sql);
-	$row=$query->fetch_assoc();
+		//query to find user
+		$sql = "SELECT * FROM user WHERE un = '$un' AND pw = '$pw'";
+		$query = $conn->query($sql);
+		$row=$query->fetch_assoc();
 
-	//check if user exists
-	if($query->num_rows > 0)
-	{
-
-	    	$_SESSION['id'] = $row['id'];
-		header('location: home.php');
-
+		//check if user exists
+		if($query->num_rows > 0){
+				$_SESSION['id'] = $row['id'];
+			header('location: home.php');
+		}
+		else{
+			echo '
+				<script>
+					alert("Incorrect username or password! Please try again.");
+					window.location = "index.php";
+				</script>
+			';
+		}
 	}
-	else
-	{
-		echo "<script src='js/sweetalert.min.js'></script>";
-	    	echo "<script>setTimeout(function(){ swal({title: 'Sila guna username / Password yang betul untuk log masuk!', 
-		icon: 'warning',timer: 3000}).then(function() {window.location = 'index.php';}); }, 1);</script>";
-	}
-}
-else if(isset($_POST['register'])){
-	echo '
-		<script>
-			alert("Incorrect username or password! Please try again.");
-			window.location = "index.php";
-		</script>
-	';
-}
+	else if(isset($_POST['register']))
+		header('location: index.php');
 ?>
