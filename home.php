68bef9c9aad7244aa8bc2eb0a1ec85873262c5c5
nabylmuhdirfan <muhammadirfannabyl@gmail.com>
2021-01-14 21:18:55 +0800

    Update home.php
    Add navigation bar and event lists for visualization.

--- home.php
+++ home.php
@@ -1,15 +1,69 @@
 <html>
+	<style>
+	*{
+		background-color: #ffffff00;
+		box-sizing: border-box;
+		font-family: 'Arial';
+		margin-left: 0;
+		margin-right: 0;
+		margin-top: 0;
+		scroll-behavior: smooth;
+	}nav a{
+		color: darkgreen;
+		text-decoration: none;
+	}.align-center{
+		width: 50%;
+		margin: auto;
+	}.box-event{
+		background-color: lightgray;
+		padding: 20px 10px 20px 10px;
+		margin: 20px auto 20px auto;
+		width: 960px;
+	}.vis{
+		border: 2px dashed red;
+	}
+	</style>
 	<head>
 		<title>STASYS - Home</title>
 		<link rel="stylesheet" type="text/css" href="styles.css">
-		<script src="https://kit.fontawesome.com/2ba9e2652f.js" crossorigin="anonymous"></script>
 	</head>
 	<body>
-		<nav align="center">
+		<!--Navigation bar-->
+		<nav align="center" class="align-center">
 			<h1>STASYS</h1>
-			<a href="user_page.php"><i class="fa fa-user-circle-o" aria-hidden="true"></i> ?username?</a>
-			<a href="event_page.php">Events</a>
-			<a href="logout.php">Log Out</a>
+			<a href="user_page.php"><i class="fa fa-user-circle-o" aria-hidden="true"></i> some_user</a>
+			<a href="event_page.php"><i class="fa fa-calendar" aria-hidden="true"></i> Events</a>
+			<a href="logout.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Log Out</a>
 		</nav>
+		<!--List existing events from Database-->
+		<div>
+			<h1>Events</h1>
+			<div class="box-event vis">
+				<b>Partyyy</b><br/><br/>
+				26/02/2021<br/><br/>
+				0/150 Joined
+				<input type="button" style="float: right" value="View"/>
+			</div>
+			<div class="box-event vis">
+				<b>Semester Break Dinner</b><br/><br/>
+				25/01/2021<br/><br/>
+				-FULL-
+				<input type="button" style="float: right" value="View"/>
+			</div>
+			<div class="box-event vis">
+				<b>Semester Break Camp</b><br/><br/>
+				25/01/2021<br/><br/>
+				0/50 Joined
+				<input type="button" style="float: right" value="View"/>
+			</div>
+			<div class="box-event vis">
+				<b>MAT263 Assessment 2</b><br/><br/>
+				15/01/2021<br/><br/>
+				0/30 Joined
+				<input type="button" style="float: right" value="View"/>
+			</div>
+		</div>
+		
+		<script src="https://kit.fontawesome.com/2ba9e2652f.js" crossorigin="anonymous"></script>
 	</body>
 </html>
