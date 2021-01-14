3931a81a27964a7352e02c3ac88fceb705f908c1
nabylmuhdirfan <muhammadirfannabyl@gmail.com>
2021-01-15 00:38:52 +0800

    Update event.php

--- event.php
+++ event.php
@@ -1,3 +1,13 @@
+<?php
+	include("config.php");
+	
+	require_once 'session_check.php';
+	
+	//fetch info for current  user
+	$sqluser = "SELECT * FROM user WHERE id ='".$_SESSION['id']."'";
+	$query_user = $conn->query($sqluser);
+	$rowuser = $query_user->fetch_assoc();
+?>
 <html>
 	<head><title>STASYS - Add Event</title>
 	<style>
