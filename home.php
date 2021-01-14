7aeef5a3a42e5698de54ac59f8b8543f58a86187
nabylmuhdirfan <muhammadirfannabyl@gmail.com>
2021-01-14 20:24:58 +0800

    Update home.php & styles.css
    Adding stuff into these two files.

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
 		<script src="https://kit.fontawesome.com/2ba9e2652f.js" crossorigin="anonymous"></script>
 	</head>
 	<body>
-		<nav>
+		<nav align="center">
 			<h1>STASYS</h1>
 			<a href="user_page.php"><i class="fa fa-user-circle-o" aria-hidden="true"></i> ?username?</a>
 			<a href="event_page.php">Events</a>
