7f46c5ea0146ab712506e2ecd68c9b03057136fc
nabylmuhdirfan <77040159+nabylmuhdirfan@users.noreply.github.com>
2021-01-17 14:53:39 +0800

    Update event_add_post.php

--- event_add_post.php
+++ event_add_post.php
@@ -12,9 +12,10 @@
 		$info=$_POST['info'];
 		$date=$_POST['date'];
 		$time=$_POST['time'];
+		$location=$_POST['place'];
 		$quota=$_POST['quota'];
 		$date_time=date('Y-m-d H:i:s', strtotime("$date $time"));
-		$sql="INSERT INTO event (title, description, date_time, quota, u_id) VALUES ('{$title}', '{$info}', '{$date_time}', '{$quota}', '{$uid}')";
+		$sql="INSERT INTO event (title, description, date_time, location quota, u_id) VALUES ('{$title}', '{$info}', '{$date_time}', '{$location}', '{$quota}', '{$uid}')";
 
 		$result=mysqli_query($conn,$sql);
 
