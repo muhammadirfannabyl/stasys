9a9ad06018e4aa5e0f1ba748a7da8db701589282
Mohd Adli <adli4632@gmail.com>
2021-01-18 22:36:56 +0800

    Update on index.php

--- index.php
+++ index.php
@@ -1,34 +1,60 @@
 <?php
-    session_start();
-    require_once "config.php";
-    //Connect to database
+session_start();
+require_once "config.php";
+//Connect to database
 ?>
 
 <html>
-    <head><title>STASYS - Login</title></head>
-    <body>
-        <h1>STASYS - LOGIN</h1>
-        
-        <form name="form1" method="post" action="login.php">
-            <table>
+
+<head>
+    <title>STASYS - Login</title>
+    <link href='https://fonts.googleapis.com/css?family=Comic Neue' rel='stylesheet'>
+</head>
+
+<style>
+    body {
+        background-color: powderblue;
+    }
+
+    h1 {
+        font-family: 'Comic Neue';
+    }
+
+    table {
+        font-family: 'Comic Neue';
+    }
+
+    button {
+        background-color: white;
+        color: black;
+        border: 2px solid #555555;
+    }
+</style>
+
+<body>
+    <h1>STASYS - LOGIN</h1>
+
+    <form name="form1" method="post" action="login.php">
+        <table>
             <tr>
                 <td>Username</td>
                 <td><input type="text" placeholder="Username" name="username" /></td>
             </tr>
-                
+
             <tr>
                 <td>Password</td>
                 <td><input type="password" placeholder="Password" name="password" /></td>
             </tr>
-                
+
             <tr>
                 <td></td>
                 <td>
-                <br><button type="submit" name="Submit" ><strong>Login</strong></button>
-                &nbsp;<button type="register" formaction="/register.php"><strong>Register</strong></button>
+                    <br><button type="submit" name="Submit"><strong>Login</strong></button>
+                    &nbsp;<button type="register" formaction="/register.php"><strong>Register</strong></button>
                 </td>
             </tr>
-            </table></br></br>
-		</form>
-    </body>
+        </table></br></br>
+    </form>
+</body>
+
 </html>
