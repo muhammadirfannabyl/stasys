d3a7c2ddf0dce93a11571e187a5684ace8429a2a
Edrie <56837896+andrianedrie@users.noreply.github.com>
2021-01-21 22:00:08 +0800

    Update register.php

--- register.php
+++ register.php
@@ -63,12 +63,6 @@
             //echo "1";
             $stmt->close();
 
-            //$success_message = "Account created successfully.";
-            //echo "<script src='js/sweetalert.min.js'></script>";
-            //echo "<script>setTimeout(function(){ swal({title: 'Success!', icon: 'success'}).then(function() {window.location = 'index.php';}); }, 1);</script>";
-
-            //$result=mysqli_query($conn,"INSERT INTO event (title, description, date_time, location, quota, u_id) VALUES ('{$title}', '{$info}', '{$date_time}', '{$location}', '{$quota}', '".$_SESSION['id']."')");
-
             if($insertSQL)
                 echo '<script>alert("SUCCESS: User has been registered successfully."); window.location = "index.php"; </script>';
             else
