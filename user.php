4b24d66c210ad49018db2424c9fddad4da7d04a8
Edrie <56837896+andrianedrie@users.noreply.github.com>
2021-02-01 01:13:42 +0800

    Update user.php

--- user.php
+++ user.php
@@ -32,9 +32,10 @@
 				<tr><td><b>Part</b></td><td>: <?php echo $rowuser['part']; ?></td></tr>
                 <tr><td><b>Date Registered</b></td><td>: <?php echo $rowuser['created']; ?></td></tr>
                 <tr><td><b>Access Level</b></td><td>: <?php echo $rowuser['access_lvl']; ?></td></tr>
+                <p id="demo"></p>
                 <script>
                     var access_lvl = parseInt($rowuser['access_lvl']);
-                    if (access_lvl == 1) <tr><tr><td><b>Acess Level</b></td><td>: Developer</td></tr>
+                    if (access_lvl == 1) document.getElementById("demo").innerHTML = "Developers";
                     if (access_lvl == 2) <tr><tr><td><b>Acess Level</b></td><td>: Admin</td></tr>
                     if (access_lvl == 3) <tr><tr><td><b>Acess Level</b></td><td>: User</td></tr>
                 </script>
