2755f3fbc24ffe3bd96da5a70cc64875345f9d61
Edrie <56837896+andrianedrie@users.noreply.github.com>
2021-02-01 00:09:03 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,36 @@
 <?php
-	session_start();
-	require_once "config.php";
-	//Connect to database
+session_start();
+require_once "config.php";
+//Connect to database
 ?>
-
 <html>
-<body>
+<head>
+    <title>STASYS - Login</title>
+    <link href='https://fonts.googleapis.com/css?family=Comic Neue' rel='stylesheet'>
+    <link rel="stylesheet" type="text/css" href="./styles.css"/>
+</head>
 
-  <form name="form1" method="post" action="login.php">
-  <table>
-                    <tr>
-                        <td>User Name</td>
-                        <td><input type="text" placeholder="Username" name="username" /></td>
-                    </tr>
-                    <tr>
-                        <td>Password</td>
-                        <td><input type="password" placeholder="Password" name="password" /></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td>
-                            <br><button type="submit" name="Submit" ><strong>Login</strong></button>
-						&nbsp;<button type="submit" name="register" ><strong>Register</strong></button>
-                        </td>
-                    </tr>
-               </table></br></br>
-
+<body class="title">
+    <div class="align-center">STASYS - LOGIN</div>
+    <form name="form1" method="post" action="login.php">
+        <table>
+            <tr>
+                <td>Username</td>
+                <td><input type="text" placeholder="Username" name="username" /></td>
+            </tr>
+            <tr>
+                <td>Password</td>
+                <td><input type="password" placeholder="Password" name="password" /></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <br><button type="submit" name="Submit"><strong>Login</strong></button>
+                    &nbsp;<button type="register" formaction="/register.php"><strong>Register</strong></button>
+                </td>
+            </tr>
+        </table></br></br>
+    </form>
 </body>
 
-</html>
+</html> 
