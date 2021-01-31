1673db0eeca34f729fb0c071fd503ce59faa88d6
Edrie <56837896+andrianedrie@users.noreply.github.com>
2021-02-01 00:06:46 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,114 +1,31 @@
 <?php
-  session_start();
-  require_once "config.php";
-  //Connect to database
+	session_start();
+	require_once "config.php";
+	//Connect to database
 ?>
 
-<!DOCTYPE html>
-<html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>STASYS - Login</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes"><link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'><link rel="stylesheet" href="./style.css">
-
-</head>
+<html>
 <body>
-<!-- partial:index.partial.html -->
-<div class="cont">
-  <div class="demo">
-    <div class="login">
-      <div class="login__check"></div>
-      <h1 style="font-size: 20px;color: rgb(253, 253, 253);text-align: center;">STASYS - LOGIN</h1>
-      <div class="login__form">
-        <div class="login__row">
-          <svg class="login__icon name svg-icon" viewBox="0 0 20 20">
-            <path d="M0,20 a10,8 0 0,1 20,0z M10,0 a4,4 0 0,1 0,8 a4,4 0 0,1 0,-8" />
-          </svg>
-          <input type="text" class="login__input name" placeholder="Username"/>
-        </div>
-        <div class="login__row">
-          <svg class="login__icon pass svg-icon" viewBox="0 0 20 20">
-            <path d="M0,20 20,20 20,8 0,8z M10,13 10,16z M4,8 a6,8 0 0,1 12,0" />
-          </svg>
-          <input type="password" class="login__input pass" placeholder="Password"/>
-        </div>
-        <button type="submit" class="Submit" name="Submit">Log in</button>
-        <p class="login__signup">Don't have an account? &nbsp;<a formaction="/register.php">Sign up</a></p>
-      </div>
-    </div>
-    <div class="app">
-      <div class="app__top">
-        <div class="app__menu-btn">
-          <span></span>
-        </div>
-        <svg class="app__icon search svg-icon" viewBox="0 0 20 20">
-          <!-- yeap, its purely hardcoded numbers straight from the head :D (same for svg above) -->
-          <path d="M20,20 15.36,15.36 a9,9 0 0,1 -12.72,-12.72 a 9,9 0 0,1 12.72,12.72" />
-        </svg>
-        <p class="app__hello">Good Morning!</p>
-        <div class="app__user">
-          <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/142996/profile/profile-512_5.jpg" alt="" class="app__user-photo" />
-          <span class="app__user-notif">3</span>
-        </div>
-        <div class="app__month">
-          <span class="app__month-btn left"></span>
-          <p class="app__month-name">March</p>
-          <span class="app__month-btn right"></span>
-        </div>
-      </div>
-      <div class="app__bot">
-        <div class="app__days">
-          <div class="app__day weekday">Sun</div>
-          <div class="app__day weekday">Mon</div>
-          <div class="app__day weekday">Tue</div>
-          <div class="app__day weekday">Wed</div>
-          <div class="app__day weekday">Thu</div>
-          <div class="app__day weekday">Fri</div>
-          <div class="app__day weekday">Sad</div>
-          <div class="app__day date">8</div>
-          <div class="app__day date">9</div>
-          <div class="app__day date">10</div>
-          <div class="app__day date">11</div>
-          <div class="app__day date">12</div>
-          <div class="app__day date">13</div>
-          <div class="app__day date">14</div>
-        </div>
-        <div class="app__meetings">
-          <div class="app__meeting">
-            <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/142996/profile/profile-80_5.jpg" alt="" class="app__meeting-photo" />
-            <p class="app__meeting-name">Feed the cat</p>
-            <p class="app__meeting-info">
-              <span class="app__meeting-time">8 - 10am</span>
-              <span class="app__meeting-place">Real-life</span>
-            </p>
-          </div>
-          <div class="app__meeting">
-            <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/142996/profile/profile-512_5.jpg" alt="" class="app__meeting-photo" />
-            <p class="app__meeting-name">Feed the cat!</p>
-            <p class="app__meeting-info">
-              <span class="app__meeting-time">1 - 3pm</span>
-              <span class="app__meeting-place">Real-life</span>
-            </p>
-          </div>
-          <div class="app__meeting">
-            <img src="//s3-us-west-2.amazonaws.com/s.cdpn.io/142996/profile/profile-512_5.jpg" alt="" class="app__meeting-photo" />
-            <p class="app__meeting-name">FEED THIS CAT ALREADY!!!</p>
-            <p class="app__meeting-info">
-              <span class="app__meeting-time">This button is just for demo ></span>
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="app__logout">
-        <svg class="app__logout-icon svg-icon" viewBox="0 0 20 20">
-          <path d="M6,3 a8,8 0 1,0 8,0 M10,0 10,12"/>
-        </svg>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- partial -->
-  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
+
+  <form name="form1" method="post" action="login.php">
+  <table>
+                    <tr>
+                        <td>User Name</td>
+                        <td><input type="text" placeholder="Username" name="username" /></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" placeholder="Password" name="password" /></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <br><button type="submit" name="Submit" ><strong>Login</strong></button>
+						&nbsp;<button type="submit" name="register" ><strong>Register</strong></button>
+                        </td>
+                    </tr>
+               </table></br></br>
 
 </body>
+
 </html>
