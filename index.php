89c71881b128b4bd290ec4581cfd6b8718f7dcb9
Edrie <56837896+andrianedrie@users.noreply.github.com>
2021-01-31 23:55:40 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,41 +1,45 @@
 <?php
-session_start();
-require_once "config.php";
-//Connect to database
+  session_start();
+  require_once "config.php";
+  //Connect to database
 ?>
 
-<html>
-
+<!DOCTYPE html>
+<html lang="en" >
 <head>
-    <title>STASYS - Login</title>
-    <link href='https://fonts.googleapis.com/css?family=Comic Neue' rel='stylesheet'>
-    <link rel="stylesheet" type="text/css" href="./styles.css"/>
+  <meta charset="UTF-8">
+  <title>STASYS - Login</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes"><link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans'><link rel="stylesheet" href="./style.css">
+
 </head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="cont">
+  <div class="demo">
+    <div class="login">
+      <div class="login__check"></div>
+      <h1 style="font-size: 20px;color: rgb(253, 253, 253);text-align: center;">STASYS - LOGIN</h1>
+      <div class="login__form">
+        <div class="login__row">
+          <svg class="login__icon name svg-icon" viewBox="0 0 20 20">
+            <path d="M0,20 a10,8 0 0,1 20,0z M10,0 a4,4 0 0,1 0,8 a4,4 0 0,1 0,-8" />
+          </svg>
+          <input type="text" class="login__input name" placeholder="Username"/>
+        </div>
+        <div class="login__row">
+          <svg class="login__icon pass svg-icon" viewBox="0 0 20 20">
+            <path d="M0,20 20,20 20,8 0,8z M10,13 10,16z M4,8 a6,8 0 0,1 12,0" />
+          </svg>
+          <input type="password" class="login__input pass" placeholder="Password"/>
+        </div>
+        <button type="submit" class="Submit" name="Submit">Log in</button>
+        <p class="login__signup">Don't have an account? &nbsp;<a formaction="/register.php">Sign up</a></p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- partial -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script><script  src="./script.js"></script>
 
-<body class="title">
-    <div class="align-center">STASYS - LOGIN</div>
-
-    <form name="form1" method="post" action="login.php">
-        <table>
-            <tr>
-                <td>Username</td>
-                <td><input type="text" placeholder="Username" name="username" /></td>
-            </tr>
-
-            <tr>
-                <td>Password</td>
-                <td><input type="password" placeholder="Password" name="password" /></td>
-            </tr>
-
-            <tr>
-                <td></td>
-                <td>
-                    <br><button type="submit" name="Submit"><strong>Login</strong></button>
-                    &nbsp;<button type="register" formaction="/register.php"><strong>Register</strong></button>
-                </td>
-            </tr>
-        </table></br></br>
-    </form>
 </body>
-
 </html>
