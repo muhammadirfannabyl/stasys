154556bf248375ad2b914ff8fa720090f4dc5be8
nabylmuhdirfan <muhammadirfannabyl@gmail.com>
2021-02-01 19:34:20 +0800

    Update index.php

--- home/index.php
+++ home/index.php
@@ -42,12 +42,12 @@
 						$count_str = $count['count']." participants joined";
 					
 					if ($rows['status'] == 0)
-						$status = "<div style='color: #7f4600;'>Approval Pending";
+						$status = "<div style='color: #7f4600;'>Approval Pending <i class='fa fa-exclamation' aria-hidden='true'></i></div>";
 					else
-						$status = "<div style='color: darkgreen;'>Approved"; ?>
+						$status = "<div style='color: darkgreen;'>Approved <i class='fa fa-check' aria-hidden='true'></i></div>"; ?>
 			
 			<div class="event-bg">
-				<div class="txt"><h1><?php echo $rows['title']; ?></h1></div>
+				<div class="txt"><h1><?php echo $rows['title']; ?></h1><?php echo $status; ?></div>
 				<div class="txt"><?php echo date('h:i A (Hi), d/m/Y', strtotime($rows['date_time'])); ?></div>
 				<div class="txt"><?php echo $count_str; ?></div>
 				<a href="./event/?no=<?php echo $rows['id']; ?>"><div class="button">View</div></a>
