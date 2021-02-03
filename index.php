f4a39bd6ec2cb842000f422284448959e995986b
nabylmuhdirfan <muhammadirfannabyl@gmail.com>
2021-02-03 19:17:23 +0800

    Add footer

--- index.php
+++ index.php
@@ -43,4 +43,5 @@
 		</div>
 		<script src="https://kit.fontawesome.com/2ba9e2652f.js" crossorigin="anonymous"></script>
 	</body>
+	<?php include './sess/footer.php';?>
 </html>
---
+++ sess/footer.php
@@ -0,0 +1,3 @@
+<footer>
+	&#169; 2020 - <?php echo date('Y'); ?> Student Activity System (STASYS).<br/>All Rights Reserved.
+</footer>
