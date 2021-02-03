76003931af0f04737452061cd55ffbb109f8fdaa
nabylmuhdirfan <muhammadirfannabyl@gmail.com>
2021-02-03 21:05:23 +0800

    Update config.php

--- sess/config.php
+++ sess/config.php
@@ -1,6 +1,6 @@
 <?php
 	$conn = new mysqli("localhost","u726414194_stasy", "Qwer1234", "u726414194_stasys");
-	$VERSION = 0.6;
+	$VERSION = 0.7;
 	if($conn->connect_error)
 		die("Connection failed: " .$conn->connect_error);
 ?>
