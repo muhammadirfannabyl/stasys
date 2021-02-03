fb3ad264cb028429fb3ffe135aec9ed6dbda3eff
nabylmuhdirfan <muhammadirfannabyl@gmail.com>
2021-02-03 19:04:21 +0800

    Style login page

--- index.php
+++ index.php
@@ -4,34 +4,43 @@
 	require_once "./sess/config.php";
 ?>
 <html>
-
+	<!--START: PAGE HEADER-->
 	<head>
 		<title>STASYS - Login</title>
-		<link href='https://fonts.googleapis.com/css?family=Comic Neue' rel='stylesheet'>
-		<link rel="stylesheet" type="text/css" href="../css/styles.css"/>
+		<link href="https://fonts.googleapis.com/css?family=Comic Neue" rel='stylesheet'/>
+		<link rel="stylesheet" type="text/css" href="../../../css/styles-new.css"/>
 	</head>
-
-	<body class="title">
-		<div class="align-center">STASYS - LOGIN</div>
-		<form method="post" action="./sess/login.php">
-			<table>
-				<tr>
-					<td>User ID</td>
-					<td><input type="text" placeholder="User ID" name="username" /></td>
-				</tr>
-				<tr>
-					<td>Password</td>
-					<td><input type="password" placeholder="Password" name="password" /></td>
-				</tr>
-				<tr>
-					<td></td>
-					<td>
-						<br><button type="submit" name="login"><p1>Log In</p1></button>
-						&nbsp;<button type="submit" name="register"><p1>Register</p1></button>
-					</td>
-				</tr>
-			</table></br></br>
-		</form>
+	<!--END: PAGE HEADER-->
+	<body>
+		<!--START: NAVBAR-->
+		<nav>
+			<a class="home" href="./"><h1>STASYS</h1> Student Activity System v <?php echo $VERSION; ?></a>
+		</nav>
+		<!--END: NAVBAR-->
+		<div class="wrapper">
+			<center><h1><strong>Log In</strong></h1></center>
+			<!--START: ADD EVENT FORM-->
+				<form method="post" action="./sess/login.php">
+					<div class="input-group">
+						<div class="half">
+							<div class="wrapper">
+								<label>User ID</label><br/>
+								<input name="username" type="text" placeholder="Enter user ID..."/>
+							</div>
+						</div>
+						<div class="half">
+							<div class="wrapper">
+								<label>Password</label><br/>
+								<input name="password" type="password" placeholder="Enter password..."/>
+							</div>
+						</div><br/><br/>
+					</div>
+					<div class="action">
+						<a href=""><button name="login" type="submit" value="Login">&nbsp;&nbsp;&nbsp;Login&nbsp;&nbsp;&nbsp;</button></a><a href="../../" required><button name="register" type="submit" value="Login">&nbsp;&nbsp;Register&nbsp;&nbsp;</button></a><a href="../../" required></a>
+					</div>
+				</form>
+			<!--END: EVENT INFORMATION-->
+		</div>
+		<script src="https://kit.fontawesome.com/2ba9e2652f.js" crossorigin="anonymous"></script>
 	</body>
-
-</html> 
+</html>
